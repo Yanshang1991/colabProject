28d417d6f2f48f35cb4325cdc083186c31af407f
张波 <261512@nd.com>
2021-05-25 22:25:28 +0800

    测试能否和colab同步

---
+++ utils/file_uitls.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+
+def exists_or_create(dir):
+    """
+    检查目录是否存在
+    如果不存在，创建一个
+    :param dir: 目录
+    """
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+
+def cp(src, tar_dir):
+    """
+    复制文件
+    :param src: 资源路径
+    :param tar_dir: 目标路径
+    """
+    assert os.path.exists(src)  # 判断文件存在
+    exists_or_create(tar_dir)
+    tar_dir = os.path.join(tar_dir, os.path.dirname(src))
+    os.makedirs(tar_dir)
+
+    # adding exception handling
+    try:
+        shutil.copy(src, tar_dir)
+    except IOError as e:
+        print("Unable to copy file. %s" % e)
