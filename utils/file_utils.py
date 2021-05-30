0410088ca91eb292a29044724f0d71f7619230d9
ZhangBo <yanshang1991@gmail.com>
2021-05-30 21:18:08 +0800

    提交代码

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -4,6 +4,23 @@ import os
 import shutil
 
 
+def cal_files(tar_dir, file_ext = None):
+    """
+    统计目录下指定类型文件的数量。递归
+    :param tar_dir: 目录
+    :param file_ext: 文件名后缀，如".wav"
+    :return: 数量
+    """
+    count = 0
+    for root, dirs, files in os.walk(tar_dir, topdown = False, followlinks = True):
+        for file in files:
+            name, ext = os.path.splitext(file)
+            if file_ext is None or ext == file_ext:
+                count += 1
+
+    return count
+
+
 def exists_or_create(dir):
     """
     检查目录是否存在
@@ -11,7 +28,7 @@ def exists_or_create(dir):
     :param dir: 目录
     """
     if not os.path.exists(dir):
-        os.mkdir(dir)
+        os.makedirs(dir)
 
 
 def cp(src_path, dst_path):
@@ -29,6 +46,33 @@ def cp(src_path, dst_path):
         print("Unable to copy file. %s" % e)
 
 
+def cpdir(src: str, dst_dir):
+    """
+    复制整个文件夹下的路径
+    :param scr_dir: 如/content/src/ /content/src
+    :param dst_path:
+    :return:
+    """
+    sum = cal_files(tar_dir = src)
+    print("文件总数：%d" % sum)
+
+    src = src.rstrip("/")  # 移除目录末尾的/
+    src_dir = os.path.dirname(src)
+    index = 0
+    for root, dirs, files in os.walk(src, topdown = True, followlinks = True):
+        pure_path = root.replace(src_dir, "").lstrip("/")
+        files_dir = os.path.join(dst_dir, pure_path)
+        for file in files:
+            cp(os.path.join(root, file), os.path.join(files_dir, file))
+
+            # 如果总数大于200个，显示复制进度
+            if sum > 200:
+                print('\r' + '[进度]:%s%.2f%%' % ('>' * int(index * 50 / sum), float(index / sum * 100)), end = ' ')
+
+
+cpdir(r"./aaaa", r"../aaaaa/bbbb/cccc")
+
+
 def mv(src, tar_dir):
     shutil.move(src, tar_dir)
 
@@ -43,22 +87,5 @@ def walk_dir(tar_dir, file_ext, callback):
     for root, dirs, files in os.walk(tar_dir, topdown = False, followlinks = True):
         for file in files:
             name, ext = os.path.splitext(file)
-            if ext == file_ext:
+            if ext is None or ext == file_ext:
                 callback(os.path.join(root, file), root, name, ext)
-
-
-def cal_files(tar_dir, file_ext):
-    """
-    统计目录下指定类型文件的数量。递归
-    :param tar_dir: 目录
-    :param file_ext: 文件名后缀，如".wav"
-    :return: 数量
-    """
-    count = 0
-    for root, dirs, files in os.walk(tar_dir, topdown = False, followlinks = True):
-        for file in files:
-            name, ext = os.path.splitext(file)
-            if ext == file_ext:
-                count += 1
-
-    return count
