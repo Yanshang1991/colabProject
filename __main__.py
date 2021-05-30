5e6d62133e693e189987a27a0b8d95162d2eb1f8
ZhangBo <yanshang1991@gmail.com>
2021-05-30 21:51:23 +0800

    提交代码

--- __main__.py
+++ __main__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import sys
+import os
+
+if not __package__:
+    path = os.path.join(os.path.dirname(__file__), os.pardir)
+    sys.path.insert(0, path)
