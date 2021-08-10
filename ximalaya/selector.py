9e4184a1d030a29e888e589d5f71a6250a22d469
ZhangBo <yanshang1991@gmail.com>
2021-08-11 01:51:48 +0800

    修改数据集

--- ximalaya/selector.py
+++ ximalaya/selector.py
@@ -1,32 +1,24 @@
 import csv
-from typing import List
-
+import os
 import download_utils as du
 
+# CSV文件路径，必填
+csv_path = f"/content/colabProject/ximalaya/一禅575-714.csv"
+# 下载目录
+output_dir = f"/content/data"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok = True)
 
-def read_from_csv(csv_path: str) -> List:
-    with open(csv_path, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-    return reader
-
-
-if __name__ == '__main__':
-
-
-
-    path = f"./一禅575-714.csv"
-    # row_list = read_from_csv(path)
-    url_list = []
-    with open(path, 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        for index, row in enumerate(reader):
-            if index == 0:
-                continue
-            if row[2].startswith("20"):
-                url = row[0]
-                if url not in url_list:
-                    url_list.append(row[0])
-    index = 84
-    for url in url_list:
-        du.single(url, f"./data", str(index) + ".m4a")
-        index += 1
+# 解析url
+url_list = []
+with open(csv_path, 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    for index, row in enumerate(reader):
+        if index == 0:
+            continue
+        if row[2].startswith("20"):
+            url = row[0]
+            if url not in url_list:
+                url_list.append(row[0])
+# 多线程下载数据
+du.many(url_list, f"/content/data", True)
